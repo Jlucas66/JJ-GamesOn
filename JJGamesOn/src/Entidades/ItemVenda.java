fd710b9d332a8ff43bfdfe7f8f65e5d1e42c1280
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-11-24 23:19:14 -0300

    Update ItemVenda
    adicionando construtor, get e set

--- JJGamesOn/src/Entidades/ItemVenda.java
+++ JJGamesOn/src/Entidades/ItemVenda.java
@@ -3,4 +3,25 @@ package Entidades;
 public class ItemVenda {
     private Jogo jogo;
     private int quantidade;
+
+    public ItemVenda(Jogo jogo, int quantidade) {
+        this.jogo = jogo;
+        this.quantidade = quantidade;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
