b50a251f12a0ddcff8ad39cc3b246a5cee7adade
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-11-24 23:20:10 -0300

    Update Avaliação
    adicionando construtor, get e set

--- JJGamesOn/src/Entidades/Avaliacao.java
+++ JJGamesOn/src/Entidades/Avaliacao.java
@@ -4,4 +4,34 @@ public class Avaliacao {
     private Cliente cliente;
     private Jogo jogo;
     private int nota;
+
+    public Avaliacao(Cliente cliente, Jogo jogo, int nota) {
+        this.cliente = cliente;
+        this.jogo = jogo;
+        this.nota = nota;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
 }
