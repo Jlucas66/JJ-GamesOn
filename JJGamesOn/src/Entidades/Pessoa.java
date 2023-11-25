95205e938bd65ae20b97f11b71d9d631258b288e
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-11-24 22:28:13 -0300

    Update Pessoa
    Adicionando construtor da classe mãe e seus respectivos getters e setters

--- JJGamesOn/src/Entidades/Pessoa.java
+++ JJGamesOn/src/Entidades/Pessoa.java
@@ -7,4 +7,43 @@ public abstract class Pessoa {
     private String email;
     private String senha;
     private LocalDateTime dataNascimento;
+
+    public Pessoa(String nome, String email, String senha, LocalDateTime dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDateTime dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
