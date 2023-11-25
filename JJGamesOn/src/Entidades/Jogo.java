903fdaa06b70c5655e7396cc6eb47271b3f77b30
Anthony <anthony_efraim@hotmail.com>
2023-11-24 23:02:38 -0300

    Adicionando atributo FaixaEtaria

--- JJGamesOn/src/Entidades/Jogo.java
+++ JJGamesOn/src/Entidades/Jogo.java
@@ -7,6 +7,21 @@ public class Jogo {
     private String resumo;
     private double notaMetacritic;
     private int codigoDeAtivacao;
+    private FaixaEtaria faixaEtaria;
+
+    public enum FaixaEtaria {
+        LIVRE(0), DEZ(10), DOZE(12), QUATORZE(14), DEZESSEIS(16), DEZOITO(18);
+
+        private final int idade;
+
+        public int getIdade() {
+            return idade;
+        }
+
+        FaixaEtaria(int idade) {
+            this.idade = idade;
+        }
+    }
 
     public void Media(){
 
