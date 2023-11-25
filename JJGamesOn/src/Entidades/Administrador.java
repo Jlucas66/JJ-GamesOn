b009d2756561feef8849f0d2d8c60c58a8780cb3
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-11-24 22:29:21 -0300

    Update Administrador
    Adicionando o construtor da classe administrador e seus respectivos getters e setters

--- JJGamesOn/src/Entidades/Administrador.java
+++ JJGamesOn/src/Entidades/Administrador.java
@@ -1,5 +1,20 @@
 package Entidades;
 
+import java.time.LocalDateTime;
+
 public class Administrador extends Pessoa{
     private int chaveEspecial;
+
+    public Administrador(String nome, String email, String senha, LocalDateTime dataNascimento, int chaveEspecial) {
+        super(nome, email, senha, dataNascimento);
+        this.chaveEspecial = chaveEspecial;
+    }
+
+    public int getChaveEspecial() {
+        return chaveEspecial;
+    }
+
+    public void setChaveEspecial(int chaveEspecial) {
+        this.chaveEspecial = chaveEspecial;
+    }
 }
