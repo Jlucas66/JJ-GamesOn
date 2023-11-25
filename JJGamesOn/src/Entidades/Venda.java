275df580facfc2c30ae5aba59eb5424f6db35615
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-11-24 22:48:39 -0300

    Update Venda
    Mudando o atributo itensVenda para um ArrayList já instanciado e criando o método para adicionar um item a venda, além de escrever o construtor da classe

--- JJGamesOn/src/Entidades/Venda.java
+++ JJGamesOn/src/Entidades/Venda.java
@@ -2,11 +2,21 @@ package Entidades;
 
 import java.lang.reflect.Array;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Venda {
     private Cliente cliente;
-    private Array itensDaVenda;
+    private ArrayList<ItemVenda> itensVenda = new ArrayList();
     private LocalDateTime data;
+
+    public Venda(Cliente cliente, LocalDateTime data) {
+        this.cliente = cliente;
+        this.data = data;
+    }
+
+    public void adicionarItemVenda(ItemVenda novoItem){
+        itensVenda.add(novoItem);
+    }
     public void calcularTotal() {
     }
 }
