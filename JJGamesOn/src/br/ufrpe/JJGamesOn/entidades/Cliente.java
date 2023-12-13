6a4200085f2804019f0e6653f9a16a1d8ffbb236
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-12-13 16:07:38 -0300

    Update Cliente
    Removendo historico de compra

--- JJGamesOn/src/br/ufrpe/JJGamesOn/entidades/Cliente.java
+++ JJGamesOn/src/br/ufrpe/JJGamesOn/entidades/Cliente.java
@@ -1,25 +1,11 @@
 package br.ufrpe.JJGamesOn.entidades;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class Cliente extends Pessoa{
-    private ArrayList<Venda> historicoDeCompras = new ArrayList<>();
 
     public Cliente(String nome, String email, String senha, LocalDate dataNascimento) {
         super(nome, email, senha, dataNascimento);
     }
 
-
-    // Esses códigos vão ficar nos repositorios, meramente ilustrativo
-    public void adicionarHistorico(Venda venda){
-        if(!historicoDeCompras.contains(venda)){
-            historicoDeCompras.add(venda);
-        }
-    }
-    public void removerHistorico(Venda venda){
-        if(historicoDeCompras.contains(venda)){
-            historicoDeCompras.remove(venda);
-        }
-    }
 }
